89055c5feb6dc436d32f715788a68fb0992fd97b
NnWinter <515621078@163.com>
2023-03-22 09:54:22 +0800

    Code generated userstories

--- Trello/UserStories/Week4/Program.cs
+++ Trello/UserStories/Week4/Program.cs
@@ -1,3 +1,14 @@
 ﻿var userStories = new UserStories.UserStories("UserStories.xml");
 
-Console.WriteLine(userStories.Stories.First().ToString());
+// 输出成文件
+var outfile = File.CreateText("UserStories_Generated.txt");
+foreach(var story in userStories.Stories)
+{
+    outfile.WriteLine(story);
+    if(story.Id != userStories.Stories.Last().Id)
+    {
+        outfile.WriteLine("\n ---- ---- ---- ---- \n");
+    }
+    outfile.Flush();
+}
+outfile.Close();
