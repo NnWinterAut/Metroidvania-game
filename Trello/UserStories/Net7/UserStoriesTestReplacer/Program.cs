5738e16846df6ce7ebbd9af936ab407d1e8a76f5
NnWinter <515621078@163.com>
2023-03-26 10:21:32 +0800

    Merge

--- Trello/UserStories/Net7/UserStoriesTestReplacer/Program.cs
+++ Trello/UserStories/Net7/UserStoriesTestReplacer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CSVFile;
+using System.Text;
 using System.Text.RegularExpressions;
 
 var dir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.Parent.Parent;
@@ -6,12 +7,18 @@ var stories = File.ReadAllText(Path.Combine(dir.FullName, "UserStories.xml"));
 
 var replacements = GetReplacement(Path.Combine(dir.FullName, "UserStories_Acceptance test.csv"));
 
+foreach (var rp in replacements)
+{
+    stories = stories.Replace(rp.Test, rp.Replace);
+}
 
-Dictionary<string, string> GetReplacement(string csv_path)
+File.WriteAllText(Path.Combine(dir.FullName, "UserStories.xml"), stories);
+
+List<Replacement> GetReplacement(string csv_path)
 {
     var csv = CSVReader.FromFile(csv_path);
 
-    var dic = new Dictionary<string, string>();
+    var list = new List<Replacement>();
     foreach (var line in csv)
     {
         var tests = Regex.Matches(line[1], "<Test>(.+)</Test>")
@@ -21,16 +28,47 @@ Dictionary<string, string> GetReplacement(string csv_path)
         var replaces = Regex.Matches(rp, "((Given|When)\\s.+(?:\\n)?)((And|When|Then)\\s.+(?:\\n)?)+((?:\\n)?(And)\\s.+(?:\\n)?)?")
            .Select(x => x.Value.Trim()).ToArray();
 
-        if(tests.Length != replaces.Length)
+        if (tests.Length != replaces.Length)
         {
             throw new Exception("数据数量不匹配: " + line[0]);
         }
 
-        for(int i=0; i<tests.Length; i++)
+        for (int i = 0; i < tests.Length; i++)
         {
-            dic.Add(tests[i], replaces[i]);
+            list.Add(new Replacement(int.Parse(line[0]), i, tests[i], replaces[i]));
         }
     }
 
-    return null;
+    return list;
+}
+
+struct Replacement
+{
+    public int Id { get; init; }
+    public int Index { get; init; }
+    public string Test { get; init; }
+    public string Replace { get; init; }
+    public Replacement(int id, int index, string test, string replace)
+    {
+        Id = id;
+        Index = index;
+        Test = test;
+        Replace = FormatReplace(replace);
+    }
+    string FormatReplace(string replace)
+    {
+        var str = new StringBuilder();
+        string[] lines = replace.Split(new[] { "\r", "\n", "\r\n" }, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        str.AppendLine();
+        for(int i=0;i<lines.Length; i++)
+        {
+            str.Append(
+                "                " + 
+                lines[i].Trim().Replace(",","").Replace(".", "") + 
+                (i==lines.Length-1 ? "." : ",") + 
+                "\n");
+        }
+        str.Append("            ");
+        return str.ToString();
+    }
 }
