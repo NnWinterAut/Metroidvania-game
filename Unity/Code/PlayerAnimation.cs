a8f88e9f7cc0d5700c0448ff702a86ba275ebce5
NnWinter <515621078@163.com>
2023-05-05 18:48:33 +0800

    Move code to folder

---
+++ Unity/Code/PlayerAnimation.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
+    private PlayerContorller playerContorller;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
+        playerContorller = GetComponent<PlayerContorller>();
+    }
+
+    private void Update()
+    {
+        SetAnimation();
+    }
+
+    public void SetAnimation()
+    {
+        anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocityY", rb.velocity.y);
+        anim.SetBool("isGround", physicsCheck.isGround);
+        anim.SetBool("isCrouch", playerContorller.isCrouch);
+    }
+}
