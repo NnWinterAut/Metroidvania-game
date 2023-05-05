4954e4fee2ca02f0aad697ddc16834dd0f230ec4
719 <rqy1234@aut.uni.ac.nz>
2023-05-05 21:30:04 +1200

    Enemy & DataSaving

---
+++ Unity/My project (3)/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //All these example loads"GamePlay"
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+    public void GoToSettingsMenu()
+    {
+        SceneManager.LoadScene("SettingMenu");
+    }
+    public void GoTomainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadGame()
+    {
+        //存档界面调用
+        GameObject mainMenu = GameObject.FindGameObjectWithTag("mainMenu").transform.gameObject;
+        GameObject saveDateUI = GameObject.FindGameObjectWithTag("SaveDateUI").transform.gameObject;
+        saveDateUI.transform.GetChild(0).gameObject.SetActive(true);
+        mainMenu.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
---
+++ Unity/My project (3)/Assets/Scripts/SaveDate.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeShow();
+    }
+    
+    //关闭存档界面，显示主界面
+    public void changeShow()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            GameObject saveDateManu = GameObject.FindGameObjectWithTag("SaveDataUI").gameObject;
+            GameObject mainMenu = GameObject.FindGameObjectWithTag("mainMenu").gameObject;
+            saveDateManu.transform.GetChild(0).gameObject.SetActive(false);
+            mainMenu.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
