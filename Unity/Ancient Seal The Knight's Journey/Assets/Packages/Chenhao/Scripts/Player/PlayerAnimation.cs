0ff5c25181d1ed9fd641937e8d5f86ff751b8651
NnWinter <515621078@163.com>
2023-05-20 16:42:37 +0800

    Merge Assets
    Merge Chenhao and Jiahao's asset to one single project
    
    Note: Namespace conflict exists.

---
+++ Unity/Ancient Seal The Knight's Journey/Assets/Packages/Chenhao/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    //GetComponent from Rigidbody 2D
+    private Rigidbody2D rb;
+
+    //GetComponent from PhysicsCheck
+    private PhysicsCheck physicsCheck;
+
+    //GetComponent from PlayerController
+    private PlayerController playerController;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        physicsCheck = GetComponent<PhysicsCheck>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        SetAnimation();
+    }
+
+    public void SetAnimation()
+    {
+        anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocityY", rb.velocity.y);
+        anim.SetBool("isGround", physicsCheck.isGround);
+        anim.SetBool("isDead", playerController.isDead);
+        anim.SetBool("isAttack", playerController.isAttack);
+    }
+
+    public void PlayHurt()
+    {
+        anim.SetTrigger("hurt");
+
+    }
+
+    public void PlayAttack()
+    {
+        anim.SetTrigger("attack");
+    }
+}
---
+++ Unity/Ancient Seal The Knight's Journey/Assets/Packages/Chenhao/Scripts/UI/PlayerStatBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatBar : MonoBehaviour
+{
+    public Image healthImage;
+
+    public Image healthDelayImage;
+
+    public Image MagicImage;
+
+
+    private void Update()
+    {
+        if (healthDelayImage.fillAmount > healthImage.fillAmount)
+        {
+            healthDelayImage.fillAmount -= Time.deltaTime;
+        }
+    }
+
+    public void OnHealthChange(float persentage)
+    {
+        healthImage.fillAmount = persentage;
+    }
+}
